8c599bd03554c08a4cb92bc0be97aff6d77c4a0d
tgoodyear3 <trevor.goodyear@gtri.gatech.edu>
2015-04-21 18:24:26 -0400

    Add de-sanitization to sample program.

--- panonymizer.c
+++ panonymizer.c
@@ -13,7 +13,7 @@
 #include <mcrypt.h>
 
 //Anonymization funtion
-uint32_t PAnonymizer_anonymize(const char * key,const uint32_t orig_addr) {
+uint32_t PAnonymizer_action(const char * key,const uint32_t orig_addr,int action) {
     
     char* IV = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     int keysize = 32;
@@ -22,7 +22,7 @@ uint32_t PAnonymizer_anonymize(const char * key,const uint32_t orig_addr) {
 
     buffer = calloc(1, buffer_len);
 
-    uint8_t m_pad[16] = {[0 ... 15] = 512};  //{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    uint8_t m_pad[16] = {[0 ... 15] = 0};
     uint8_t rin_output[16];
     uint8_t rin_input[16];
     memcpy(rin_input, m_pad, 16);
@@ -57,7 +57,12 @@ uint32_t PAnonymizer_anonymize(const char * key,const uint32_t orig_addr) {
         }
         //Encryption: The Rijndael cipher is used as pseudorandom function. During each 
         //round, only the first bit of rin_output is used.
-        int r = encrypt(rin_output,buffer_len,IV,key,keysize);
+        int r;
+        if(action == 1) {
+            r = encrypt(rin_output,buffer_len,IV,key,keysize);
+        } else {
+            r = decrypt(rin_output,buffer_len,IV,key,keysize);
+        }
       	
         if(r != 0){
       		printf("Encrypt returned %d", r);
@@ -68,6 +73,14 @@ uint32_t PAnonymizer_anonymize(const char * key,const uint32_t orig_addr) {
     }
     //XOR the orginal address with the pseudorandom one-time-pad
     return result ^ orig_addr;
+}
+
+uint32_t PAnonymizer_anonymize(const char * key,const uint32_t orig_addr) {
+    return PAnonymizer_action(key,orig_addr,1);
+}
+
+uint32_t PAnonymizer_deanonymize(const char * key,const uint32_t orig_addr) {
+    return PAnonymizer_action(key,orig_addr,0);
 }
 
 // https://gist.github.com/bricef/2436364
@@ -107,13 +120,4 @@ int decrypt(
   mcrypt_module_close(td);
   
   return 0;
-}
- 
-
-void display(char* ciphertext, int len){
-  int v;
-  for (v=0; v<len; v++){
-    printf("%d ", ciphertext[v]);
-  }
-  printf("\n");
 }
--- sample.c
+++ sample.c
@@ -14,10 +14,10 @@ int main(int argc, char * argv[]) {
     unsigned char my_key[32] = "inncinncinncinncinncinncinncinnc";
 
     FILE * f;
-    unsigned int raw_addr, anonymized_addr;
+    unsigned int raw_addr, anonymized_addr, deanonymized_addr;
 
     float packet_time;
-    unsigned int packet_size, packet_addr1, packet_addr2, packet_addr3, packet_addr4, orig_addr1, orig_addr2, orig_addr3, orig_addr4;
+    unsigned int packet_size, packet_addr1, packet_addr2, packet_addr3, packet_addr4, orig_addr1, orig_addr2, orig_addr3, orig_addr4, dec1, dec2, dec3, dec4;
 
     if (argc != 2) {
       fprintf(stderr, "usage: sample raw-trace-file\n");
@@ -29,7 +29,7 @@ int main(int argc, char * argv[]) {
       exit(-2);
     }
     
-    printf("Raw \t\tSanitized \tRaw Int \tAnon Int\n");
+    printf("Raw \t\tSanitized \tDe-Sanitized \t\tRaw Int \tAnon Int \tDe-Anon Int\n");
     //readin and handle each line of the input file
     while  (fscanf(f, "%f", &packet_time) != EOF) {
     	fscanf(f, "%u", &packet_size);
@@ -47,8 +47,19 @@ int main(int argc, char * argv[]) {
     	packet_addr3 = (anonymized_addr << 16) >> 24;
     	packet_addr4 = (anonymized_addr << 24) >> 24;
 
+	// Attempt de-anonymization to ensure reversibility	
+        deanonymized_addr = PAnonymizer_deanonymize(my_key,raw_addr);
+        dec1 = deanonymized_addr >> 24;
+        dec2 = (deanonymized_addr << 8) >> 24;
+        dec3 = (deanonymized_addr << 16) >> 24;
+        dec4 = (deanonymized_addr << 24) >> 24;
+
     	//output the sanitized trace
-    	printf("%u.%u.%u.%u\t%u.%u.%u.%u\t%u\t%u\n",  orig_addr1, orig_addr2, orig_addr3, orig_addr4, packet_addr1, packet_addr2, packet_addr3, packet_addr4, raw_addr, anonymized_addr );
+    	char orig_addr_str[15], san_addr_str[15], desan_addr_str[15];
+	sprintf(orig_addr_str,"%u.%u.%u.%u",orig_addr1, orig_addr2, orig_addr3, orig_addr4);
+        sprintf(san_addr_str,"%u.%u.%u.%u",packet_addr1, packet_addr2, packet_addr3, packet_addr4);
+	sprintf(desan_addr_str,"%u.%u.%u.%u",dec1, dec2, dec3, dec4);
+    	printf("%-15s %-15s %-15s \t%-11u\t%-11u\t%-11u\n",  orig_addr_str, san_addr_str, desan_addr_str, raw_addr, anonymized_addr, deanonymized_addr );
     }
 
 }
